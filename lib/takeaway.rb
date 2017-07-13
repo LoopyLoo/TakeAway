d838d930ef50092c38e281c5a341e34477957d84
Peter Apostolou <p.p.a@btinternet.com>
2017-07-13 07:58:41 +0100

    first draft

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,4 +1,47 @@
 class Takeaway
 
+  def initialize()
+  	@meals={ "curry" => 15,"chips" => 12,"eggs" => 25 }
+	#@meals={ "curry" => 15,"chips" => 12,"sausages and beans" => 25 }
+	#hsh = colors = { "red" => 0xf00, "green" => 0x0f0, "blue" => 0x00f }
+	@choices={'Item'=> 'price' } 
+  end
+
+  def show_items()
+      @meals.keys      
+  end
+
+  def enter_choice
+
+  	
+  	begin  
+  	'please enter '
+  	input=gets.chomp
+  	puts "Please enter x to quit anything else to continue"
+  	puts input
+  	  puts 'please enter number of the meal ... '
+  	  item_number=gets.chomp
+  	  puts '  and the number of these you require'
+  	  number_of=gets.chomp
+  	  "item #{item_number} number #{number_of}"  
+  	  @choices[item_number] =number_of
+   	end while input==0
+  end
+
+  def get_choices
+  	  	 
+  	  puts 'please enter number of the meal ... '
+  	  item_number=gets.chomp
+  	  puts '  and the number of these you require'
+  	  number_of=gets.chomp
+  	  "item #{item_number} number #{number_of}"  
+  	  @choices[item_number] =number_of
+  	  #input=item_number
+  	end
+  	def list_choices 
+  		@choice.each do |key,value|
+  			puts #{key}
+  		end
+  	end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,5 +1,27 @@
 require 'takeaway'
 
-as takeaway do 
-	
+describe 'testing akeaway' do 
+	context 'takeaway functions' do
+		tk=Takeaway.new
+		it 'checks lists items from menu' do
+			#tk=Takeaway.new
+			
+			expect(tk.show_items).to eq ["curry", "chips", "eggs"]
+			
+		end
+
+		it 'takes input from the user' do
+			expect(tk.enter_choice).to eq 'curry => 5, chips=>7'
+		end
+
+		it 'takes in values and adds them to the hash' do
+
+			expect(tk.enter_choice).to eq 3
+		end
+
+		it 'lists choices' do 
+			expect(tk.list_choices).to eq 5
+		end
+
+	end
 end
